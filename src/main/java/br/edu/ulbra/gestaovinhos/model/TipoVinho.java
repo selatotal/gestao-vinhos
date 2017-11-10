9dff2bb4ddd9e8edf3e9364d3f76ffebd42e5173
Tales Viegas <selatotal@gmail.com>
2017-11-10 20:31:47 -0200

    Adicao de unicidade na descrição

--- src/main/java/br/edu/ulbra/gestaovinhos/model/TipoVinho.java
+++ src/main/java/br/edu/ulbra/gestaovinhos/model/TipoVinho.java
@@ -9,7 +9,7 @@ public class TipoVinho {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     String descricao;
 
     public Long getId() {
